28e6e44effa71961877d208d9a2c9f3720cea542
Andre <andreafragnelli@gmail.com>
2024-06-25 13:26:43 +0200

    Aggiunto il responsive

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,11 +5,14 @@ import Footer from "../components/Footer/Footer";
 import { useHomeArticles } from "../hooks/useHomeArticles";
 
 const HomePage = () => {
-  
-
   const { data: articles, isLoading, error } = useHomeArticles();
 
-  if (isLoading) return <div className={styles.imgLoading}><img src="/loading.gif" alt="loading..." /></div>;
+  if (isLoading)
+    return (
+      <div className={styles.LoadingIcon}>
+        <img src="/loading.gif" alt="loading..." />
+      </div>
+    );
   if (error) return <div>Error loading articles</div>;
 
   return (
